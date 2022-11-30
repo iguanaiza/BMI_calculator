85bcab4a573382022f5775a57185a373b1ce4948
Iza <mrs.master.of.disaster@gmail.com>
2022-11-30 21:51:31 +0100

    Update index.js
    Change from 
    var bmi = Math.round((weight) / (height * height));
    to
    var bmi = Math.round((weight) / Math.pow(height, 2));

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //The first parameter should be the weight and the second should be the height.
 function bmiCalculator(weight, height) {
 
-    var bmi = Math.round((weight) / (height*height));
+    var bmi = Math.round((weight) / Math.pow(height, 2));
     return bmi;
 }
 

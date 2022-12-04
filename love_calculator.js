c11692be244f3e183694aadd64b3cb91530c39f5
Iza <mrs.master.of.disaster@gmail.com>
2022-12-04 20:17:28 +0100

    Update love_calculator.js

--- love_calculator.js
+++ love_calculator.js
@@ -4,4 +4,27 @@ var crushName = prompt("What is your crush's name?");
 var match =Math.random()*100;
 match = Math.floor(match)+1;
 //console.log(match);
-alert(userName + " and " + crushName + " is " + match + "% match.");
+
+if (match === 100){
+    alert(userName + " and " + crushName + " is " + match + "% match. You are just meant to be together.");
+}
+
+if (match < 100 && match >=90){
+    alert(userName + " and " + crushName + " is " + match + "% match. You are awesome!");
+}
+
+if (match <90 && match >= 70){
+    alert(userName + " and " + crushName + " is " + match + "% match. That's pretty nice!");
+}
+
+if (match <70 && match >= 40){
+    alert(userName + " and " + crushName + " is " + match + "% match.");
+}
+
+if (match <40 && match >0){
+    alert(userName + " and " + crushName + " is " + match + "% match. Well, maybe think about it again?");
+}
+
+if (match === 0){
+    alert(userName + " and " + crushName + " is " + match + "% match. Definitely NOT. Run!");
+}

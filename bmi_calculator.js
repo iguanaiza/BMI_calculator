7e9aab311e49763b3afb4419827e32895f8c43d0
Iza <mrs.master.of.disaster@gmail.com>
2022-12-04 20:24:04 +0100

    Update bmi_calculator.js

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -2,15 +2,18 @@
 function bmiCalculator(weight, height) {
 
     var bmi = Math.round((weight) / Math.pow(height, 2));
-    return bmi;
+
+    if (bmi < 18.5){
+        return "Your BMI is " + bmi + ", so you are underweight.";
+    }
+    
+    if (bmi <= 24.90 && bmi >=18.5){
+        return "Your BMI is " + bmi + ", so you have a normal weight.";
+    }
+    
+        if (bmi > 24.90){
+        return "Your BMI is " + bmi + ", so you are overweight";
+    }
 }
 
-var bmi = bmiCalculator(65, 1.8); 
-
-/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
-
-var bmi = bmiCalculator(65, 1.8); 
-
-bmi should equal 20 when it's rounded to the nearest whole number.
-
-*/
+var bmi = bmiCalculator(65, 1.8);

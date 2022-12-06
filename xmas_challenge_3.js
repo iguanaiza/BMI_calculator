1ee356e49e8d5576f6726f3d02072a70fc49ab2f
Iza <mrs.master.of.disaster@gmail.com>
2022-12-06 14:11:26 +0100

    Create xmas_challenge_3.js

---
+++ xmas_challenge_3.js
@@ -0,0 +1,37 @@
+/**
+https://github.com/wswieciekodu/ChristmasChallenge
+Christmas Challenge @wswieciekodu
+     * Day 3.
+     * Santa decided to enlist the help of modern technology this year.
+     * Instead of buying a Christmas tree and ornaments, he decided to use those generated in the console (terminal).
+     * Help Santa! Prepare the code that will write a Christmas tree on the screen.
+     * If you can, do not write the tree directly in printy - mess with loops.
+     * Prepare a function that takes the height of the Christmas tree and prints the tree on the screen.
+     * So that the Christmas tree generates itself! The Christmas tree can simply be a triangle, or it can be composed of several layers. :)
+*/
+
+
+/* TO FIX
+function xmasTree(height) {
+
+    for ( let x = 0 ; x < height; x++) {
+        
+        for ( let y = 0; y <= height * 2; y++) {
+            
+             if (x >= height - y && x >= y - height) {
+                 
+                 document.write("*");
+                 
+                } 
+             
+             else document.write(" ");
+        }
+        
+    document.write("<br>");
+        
+    }
+
+}
+
+xmasTree(5);
+*/

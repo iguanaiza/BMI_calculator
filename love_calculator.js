c6f7b2edd5a354ba4d9dd79f1588045d5f5abb81
Iza <mrs.master.of.disaster@gmail.com>
2023-03-06 11:17:19 +0100

    Replaced var with let
    I learned that var shouldn't be used.

--- love_calculator.js
+++ love_calculator.js
@@ -1,8 +1,8 @@
 //Love calculator - random match %
-var userName = prompt("What is your name?");
-var crushName = prompt("What is your crush's name?");
-var match =Math.random()*100;
-match = Math.floor(match)+1;
+let userName = prompt("What is your name?");
+let crushName = prompt("What is your crush's name?");
+let match = Math.floor(Math.random()*100)+1;
+
 //console.log(match);
 
 if (match === 100){

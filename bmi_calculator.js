4517f9948cc14fde5c64687b2047d7e9f206335b
Iza <mrs.master.of.disaster@gmail.com>
2023-03-06 11:13:13 +0100

    Replacing var with let
    I learned that var shouldn't be used.

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,7 +1,7 @@
 //The first parameter should be the weight and the second should be the height.
 function bmiCalculator(weight, height) {
 
-    var bmi = Math.round((weight) / Math.pow(height, 2));
+    let bmi = Math.round((weight) / Math.pow(height, 2));
 
     if (bmi < 18.5){
         return "Your BMI is " + bmi + ", so you are underweight.";
@@ -16,4 +16,4 @@ function bmiCalculator(weight, height) {
     }
 }
 
-var bmi = bmiCalculator(65, 1.8);
+bmiCalculator(65, 1.8);
